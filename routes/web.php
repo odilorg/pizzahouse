b81a1ee89e59c505f1ddfe97093edcadb7c61ecb
Jon <odilorg@gmail.com>
2020-11-29 14:24:23 +0500

    layout finished

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,11 @@ Route::get('/', function () {
 });
 
 Route::get('/pizzas', function () {
-    return view('pizzas');
+    $pizzas = [
+        ['type' => 'hawaian', 'base' => 'cheesy crust'],
+        ['type' => 'volcano', 'base' => 'garlic crust'],
+        ['type' => 'veg supreme', 'base' => 'thin & crispy']
+
+    ];
+    return view('pizzas', ['pizzas' => $pizzas] );
 });
