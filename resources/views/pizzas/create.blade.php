b6375848d08e51405caf56aa033765dcc53c26b1
Jon <odilorg@gmail.com>
2020-11-30 15:24:58 +0500

    Update create.blade.php

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -4,7 +4,7 @@
         <div class="flex-center position-ref full-height">
          <div class="content">
                 <div class="title m-b-md">
-                    Add  a New Pizza
+                    Add a New Pizza
                 </div>
                 
 
